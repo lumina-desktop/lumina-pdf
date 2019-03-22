f0f69e0204005c29454159e74090bee6767beec0
Ken Moore <ken@ixsystems.com>
2019-03-22 11:30:43 -0400

    Fix up the lumina-pdf FreeBSD port. All good to go now.

--- pkg/mkport.sh
+++ pkg/mkport.sh
@@ -92,13 +92,11 @@ if [ $? -ne 0 ] ; then
   exit 1
 fi
 
-# Remove the pkg-plist file (auto-generated at build time now)
-if [ -e "pkg-plist" ] ; then
-  rm "pkg-plist"
+if [ ! -e "pkg-plist" ] ; then
+  make stage
+  make makeplist | grep -v "check/what/makeplist/gives/you" > pkg-plist
+  make clean
 fi
-#make stage
-#make makeplist | grep -v "check/what/makeplist/gives/you" > pkg-plist
-#make clean
 
 # Update port cat Makefile
 tcat=$(echo $port | cut -d '/' -f 1)

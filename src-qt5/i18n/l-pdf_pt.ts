7bb605d24a58da5f70a22df11d3ef773075463c2
Sérgio Marques <smarquespt@gmail.com>
2020-02-14 12:13:59 +0000

    Portuguese update

--- src-qt5/i18n/l-pdf_pt.ts
+++ src-qt5/i18n/l-pdf_pt.ts
@@ -1,237 +1,237 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="pt_BR">
+<TS version="2.1" language="pt">
 <context>
     <name>MainUI</name>
     <message>
         <location filename="../mainUI.ui" line="83"/>
         <location filename="../mainUI.ui" line="405"/>
         <source>Bookmarks</source>
-        <translation type="unfinished"></translation>
+        <translation>Marcadores</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="113"/>
         <source>Find...</source>
-        <translation type="unfinished"></translation>
+        <translation>Localizar...</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="189"/>
         <source>&amp;File</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Ficheiro</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="201"/>
         <source>&amp;Presentation</source>
-        <translation type="unfinished"></translation>
+        <translation>A&amp;presentação</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="209"/>
         <source>Edit</source>
-        <translation type="unfinished"></translation>
+        <translation>Editar</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="224"/>
         <source>View</source>
-        <translation type="unfinished"></translation>
+        <translation>Ver</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="250"/>
         <source>toolBar</source>
-        <translation type="unfinished"></translation>
+        <translation>Barra de ferramentas</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="276"/>
         <location filename="../mainUI.cpp" line="503"/>
         <source>Open PDF</source>
-        <translation type="unfinished"></translation>
+        <translation>Abrir PDF</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="279"/>
         <source>Ctrl+O</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+O</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="284"/>
         <source>Close</source>
-        <translation type="unfinished"></translation>
+        <translation>Fechar</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="287"/>
         <source>Ctrl+Q</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+Q</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="295"/>
         <source>Fit Page</source>
-        <translation type="unfinished"></translation>
+        <translation>Ajustar à página</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="303"/>
         <source>Fit Width</source>
-        <translation type="unfinished"></translation>
+        <translation>Ajustar à largura</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="308"/>
         <source>Print</source>
-        <translation type="unfinished"></translation>
+        <translation>Imprimir</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="311"/>
         <source>Ctrl+P</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+P</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="319"/>
         <source>Single Page</source>
-        <translation type="unfinished"></translation>
+        <translation>Uma página</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="327"/>
         <source>Dual Pages</source>
-        <translation type="unfinished"></translation>
+        <translation>Duas páginas</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="335"/>
         <source>All Pages</source>
-        <translation type="unfinished"></translation>
+        <translation>Todas as páginas</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="340"/>
         <location filename="../mainUI.cpp" line="160"/>
         <source>Start Presentation (at beginning)</source>
-        <translation type="unfinished"></translation>
+        <translation>Iniciar apresentação (do início)</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="350"/>
         <source>Find</source>
-        <translation type="unfinished"></translation>
+        <translation>Localizar</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="355"/>
         <source>Find Next</source>
-        <translation type="unfinished"></translation>
+        <translation>Localizar seguinte</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="360"/>
         <source>Find Previous</source>
-        <translation type="unfinished"></translation>
+        <translation>Localizar anterior</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="365"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Definições</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="370"/>
         <source>Zoom In</source>
-        <translation type="unfinished"></translation>
+        <translation>Ampliar</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="375"/>
         <source>Zoom Out</source>
-        <translation type="unfinished"></translation>
+        <translation>Reduzir</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="380"/>
         <location filename="../mainUI.cpp" line="149"/>
         <source>First Page</source>
-        <translation type="unfinished"></translation>
+        <translation>Primeira página</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="385"/>
         <location filename="../mainUI.cpp" line="150"/>
         <source>Previous Page</source>
-        <translation type="unfinished"></translation>
+        <translation>Página anterior</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="390"/>
         <location filename="../mainUI.cpp" line="151"/>
         <source>Next Page</source>
-        <translation type="unfinished"></translation>
+        <translation>Página seguinte</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="395"/>
         <location filename="../mainUI.cpp" line="152"/>
         <source>Last Page</source>
-        <translation type="unfinished"></translation>
+        <translation>Última página</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="400"/>
         <source>Properties</source>
-        <translation type="unfinished"></translation>
+        <translation>Propriedades</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="410"/>
         <source>Rotate Counterclockwise</source>
-        <translation type="unfinished"></translation>
+        <translation>Rodar à esquerda</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="415"/>
         <source>Rotate Clockwise</source>
-        <translation type="unfinished"></translation>
+        <translation>Rodar à direita</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="429"/>
         <source>Scroll Mode</source>
-        <translation type="unfinished"></translation>
+        <translation>Modo de deslocação</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="444"/>
         <location filename="../mainUI.cpp" line="158"/>
         <source>Start Presentation (current slide)</source>
-        <translation type="unfinished"></translation>
+        <translation>Iniciar apresentação (página atual)</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="452"/>
         <source>Select Mode</source>
-        <translation type="unfinished"></translation>
+        <translation>Modo de seleção</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="457"/>
         <source>Clear Highlights</source>
-        <translation type="unfinished"></translation>
+        <translation>Limpar destaques</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="345"/>
         <location filename="../mainUI.cpp" line="162"/>
         <source>Stop Presentation</source>
-        <translation type="unfinished"></translation>
+        <translation>Parar apresentação</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="241"/>
         <source>Unlock PDF</source>
-        <translation type="unfinished"></translation>
+        <translation>Desbloquear PDF</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="241"/>
         <source>Password:</source>
-        <translation type="unfinished"></translation>
+        <translation>Palavra-passe:</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="283"/>
         <source>%1 (%2)</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 (%2)</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="286"/>
         <source>Select Screen</source>
-        <translation type="unfinished"></translation>
+        <translation>Selecionar ecrã</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="286"/>
         <source>Screen:</source>
-        <translation type="unfinished"></translation>
+        <translation>Ecrã:</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="503"/>
         <source>PDF Documents (*.pdf)</source>
-        <translation type="unfinished"></translation>
+        <translation>Documentos PDF (*.pdf)</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="515"/>
         <location filename="../mainUI.cpp" line="531"/>
         <source>Page %1 of %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Página %1 de %2</translation>
     </message>
 </context>
 <context>
@@ -240,95 +240,95 @@
         <location filename="../propDialog.ui" line="14"/>
         <location filename="../propDialog.cpp" line="15"/>
         <source>PDF Information</source>
-        <translation type="unfinished"></translation>
+        <translation>Informações do PDF</translation>
     </message>
     <message>
         <location filename="../propDialog.ui" line="20"/>
         <location filename="../propDialog.cpp" line="21"/>
         <source>Title:</source>
-        <translation type="unfinished"></translation>
+        <translation>Título:</translation>
     </message>
     <message>
         <location filename="../propDialog.ui" line="70"/>
         <location filename="../propDialog.cpp" line="22"/>
         <source>Subject:</source>
-        <translation type="unfinished"></translation>
+        <translation>Assunto:</translation>
     </message>
     <message>
         <location filename="../propDialog.ui" line="120"/>
         <location filename="../propDialog.cpp" line="23"/>
         <source>Author:</source>
-        <translation type="unfinished"></translation>
+        <translation>Autor:</translation>
     </message>
     <message>
         <location filename="../propDialog.ui" line="170"/>
         <location filename="../propDialog.cpp" line="24"/>
         <source>Creator:</source>
-        <translation type="unfinished"></translation>
+        <translation>Criador:</translation>
     </message>
     <message>
         <location filename="../propDialog.ui" line="223"/>
         <location filename="../propDialog.cpp" line="25"/>
         <source>Producer:</source>
-        <translation type="unfinished"></translation>
+        <translation>Produtor:</translation>
     </message>
     <message>
         <location filename="../propDialog.ui" line="270"/>
         <location filename="../propDialog.cpp" line="26"/>
         <source>Keywords:</source>
-        <translation type="unfinished"></translation>
+        <translation>Palavras-chave:</translation>
     </message>
     <message>
         <location filename="../propDialog.ui" line="329"/>
         <source>Created: </source>
-        <translation type="unfinished"></translation>
+        <translation>Criado: </translation>
     </message>
     <message>
         <location filename="../propDialog.ui" line="388"/>
         <source>Modified: </source>
-        <translation type="unfinished"></translation>
+        <translation>Modificado: </translation>
     </message>
     <message>
         <location filename="../propDialog.ui" line="447"/>
         <source>Page Size:</source>
-        <translation type="unfinished"></translation>
+        <translation>Tamanho da página:</translation>
     </message>
     <message>
         <location filename="../propDialog.ui" line="454"/>
         <source>Number of Pages:</source>
-        <translation type="unfinished"></translation>
+        <translation>Número de páginas:</translation>
     </message>
     <message>
         <location filename="../propDialog.ui" line="476"/>
         <location filename="../propDialog.cpp" line="31"/>
         <source>Save</source>
-        <translation type="unfinished"></translation>
+        <translation>Guardar</translation>
     </message>
     <message>
         <location filename="../propDialog.ui" line="483"/>
         <location filename="../propDialog.cpp" line="32"/>
         <source>Close</source>
-        <translation type="unfinished"></translation>
+        <translation>Fechar</translation>
     </message>
     <message>
         <location filename="../propDialog.cpp" line="27"/>
         <source>Created:</source>
-        <translation type="unfinished"></translation>
+        <translation>Criado:</translation>
     </message>
     <message>
         <location filename="../propDialog.cpp" line="28"/>
         <source>Modified:</source>
-        <translation type="unfinished"></translation>
+        <translation>Modificado:</translation>
     </message>
     <message>
         <location filename="../propDialog.cpp" line="29"/>
         <source>Page Size: </source>
-        <translation type="unfinished"></translation>
+        <translation>Tamanho da página: </translation>
     </message>
     <message>
         <location filename="../propDialog.cpp" line="30"/>
         <source>Number of Pages: </source>
-        <translation type="unfinished"></translation>
+        <translation>Número de páginas: </translation>
     </message>
 </context>
 <context>
@@ -336,67 +336,67 @@
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="608"/>
         <source>Multimedia</source>
-        <translation type="unfinished"></translation>
+        <translation>Multimédia</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="609"/>
         <source>Development</source>
-        <translation type="unfinished"></translation>
+        <translation>Desenvolvimento</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="610"/>
         <source>Education</source>
-        <translation type="unfinished"></translation>
+        <translation>Educação</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="611"/>
         <source>Games</source>
-        <translation type="unfinished"></translation>
+        <translation>Jogos</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="612"/>
         <source>Graphics</source>
-        <translation type="unfinished"></translation>
+        <translation>Gráficos</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="613"/>
         <source>Network</source>
-        <translation type="unfinished"></translation>
+        <translation>Rede</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="614"/>
         <source>Office</source>
-        <translation type="unfinished"></translation>
+        <translation>Escritório</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="615"/>
         <source>Science</source>
-        <translation type="unfinished"></translation>
+        <translation>Ciência</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="616"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Definições</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="617"/>
         <source>System</source>
-        <translation type="unfinished"></translation>
+        <translation>Sistema</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="618"/>
         <source>Utility</source>
-        <translation type="unfinished"></translation>
+        <translation>Utilitários</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="619"/>
         <source>Wine</source>
-        <translation type="unfinished"></translation>
+        <translation>Wine</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="620"/>
         <source>Unsorted</source>
-        <translation type="unfinished"></translation>
+        <translation>Sem grupo</translation>
     </message>
 </context>
 </TS>

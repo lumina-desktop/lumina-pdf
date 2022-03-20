c887efa52a39493de8a6d9b5236cd0b35c10a349
Milo Ivir <43657314+milotype@users.noreply.github.com>
2022-03-20 21:45:59 +0100

    Croatian translation

--- src-qt5/i18n/l-pdf_hr.ts
+++ src-qt5/i18n/l-pdf_hr.ts
@@ -7,231 +7,231 @@
         <location filename="../mainUI.ui" line="83"/>
         <location filename="../mainUI.ui" line="405"/>
         <source>Bookmarks</source>
-        <translation type="unfinished"></translation>
+        <translation>Straničnici</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="113"/>
         <source>Find...</source>
-        <translation type="unfinished"></translation>
+        <translation>Pronađi …</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="189"/>
         <source>&amp;File</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Datoteka</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="201"/>
         <source>&amp;Presentation</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Prezentacija</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="209"/>
         <source>Edit</source>
-        <translation type="unfinished"></translation>
+        <translation>Uredi</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="224"/>
         <source>View</source>
-        <translation type="unfinished"></translation>
+        <translation>Prikaz</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="250"/>
         <source>toolBar</source>
-        <translation type="unfinished"></translation>
+        <translation>alatna traka</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="276"/>
         <location filename="../mainUI.cpp" line="503"/>
         <source>Open PDF</source>
-        <translation type="unfinished"></translation>
+        <translation>Otvori PDF</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="279"/>
         <source>Ctrl+O</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+O</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="284"/>
         <source>Close</source>
-        <translation type="unfinished"></translation>
+        <translation>Zatvori</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="287"/>
         <source>Ctrl+Q</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+Q</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="295"/>
         <source>Fit Page</source>
-        <translation type="unfinished"></translation>
+        <translation>Prilagodi stranicu</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="303"/>
         <source>Fit Width</source>
-        <translation type="unfinished"></translation>
+        <translation>Prilagodi širinu</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="308"/>
         <source>Print</source>
-        <translation type="unfinished"></translation>
+        <translation>Ispiši</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="311"/>
         <source>Ctrl+P</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+P</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="319"/>
         <source>Single Page</source>
-        <translation type="unfinished"></translation>
+        <translation>Jedna stranica</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="327"/>
         <source>Dual Pages</source>
-        <translation type="unfinished"></translation>
+        <translation>Dvije stranice</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="335"/>
         <source>All Pages</source>
-        <translation type="unfinished"></translation>
+        <translation>Sve stranice</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="340"/>
         <location filename="../mainUI.cpp" line="160"/>
         <source>Start Presentation (at beginning)</source>
-        <translation type="unfinished"></translation>
+        <translation>Pokreni prezentaciju (od početka)</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="350"/>
         <source>Find</source>
-        <translation type="unfinished"></translation>
+        <translation>Pronađi</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="355"/>
         <source>Find Next</source>
-        <translation type="unfinished"></translation>
+        <translation>Pronađi sljedeće</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="360"/>
         <source>Find Previous</source>
-        <translation type="unfinished"></translation>
+        <translation>Pronađi prethodno</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="365"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Postavke</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="370"/>
         <source>Zoom In</source>
-        <translation type="unfinished"></translation>
+        <translation>Uvećaj prikaz</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="375"/>
         <source>Zoom Out</source>
-        <translation type="unfinished"></translation>
+        <translation>Umanji prikaz</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="380"/>
         <location filename="../mainUI.cpp" line="149"/>
         <source>First Page</source>
-        <translation type="unfinished"></translation>
+        <translation>Prva stranica</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="385"/>
         <location filename="../mainUI.cpp" line="150"/>
         <source>Previous Page</source>
-        <translation type="unfinished"></translation>
+        <translation>Prethodna stranica</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="390"/>
         <location filename="../mainUI.cpp" line="151"/>
         <source>Next Page</source>
-        <translation type="unfinished"></translation>
+        <translation>Sljedeća stranica</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="395"/>
         <location filename="../mainUI.cpp" line="152"/>
         <source>Last Page</source>
-        <translation type="unfinished"></translation>
+        <translation>Zadnja stranica</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="400"/>
         <source>Properties</source>
-        <translation type="unfinished"></translation>
+        <translation>Svojstva</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="410"/>
         <source>Rotate Counterclockwise</source>
-        <translation type="unfinished"></translation>
+        <translation>Okreni nalijevo</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="415"/>
         <source>Rotate Clockwise</source>
-        <translation type="unfinished"></translation>
+        <translation>Okreni nadesno</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="429"/>
         <source>Scroll Mode</source>
-        <translation type="unfinished"></translation>
+        <translation>Modus klizanja</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="444"/>
         <location filename="../mainUI.cpp" line="158"/>
         <source>Start Presentation (current slide)</source>
-        <translation type="unfinished"></translation>
+        <translation>Pokreni prezentaciju (trenutačni slajd)</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="452"/>
         <source>Select Mode</source>
-        <translation type="unfinished"></translation>
+        <translation>Odaberi modus</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="457"/>
         <source>Clear Highlights</source>
-        <translation type="unfinished"></translation>
+        <translation>Ukloni isticanja</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="345"/>
         <location filename="../mainUI.cpp" line="162"/>
         <source>Stop Presentation</source>
-        <translation type="unfinished"></translation>
+        <translation>Prekini prezentaciju</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="241"/>
         <source>Unlock PDF</source>
-        <translation type="unfinished"></translation>
+        <translation>Otključaj PDF</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="241"/>
         <source>Password:</source>
-        <translation type="unfinished"></translation>
+        <translation>Lozinka:</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="283"/>
         <source>%1 (%2)</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 (%2)</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="286"/>
         <source>Select Screen</source>
-        <translation type="unfinished"></translation>
+        <translation>Odaberi ekran</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="286"/>
         <source>Screen:</source>
-        <translation type="unfinished"></translation>
+        <translation>Ekran:</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="503"/>
         <source>PDF Documents (*.pdf)</source>
-        <translation type="unfinished"></translation>
+        <translation>PDF dokumenti (*.pdf)</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="515"/>
         <location filename="../mainUI.cpp" line="531"/>
         <source>Page %1 of %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Stranica %1 od %2</translation>
     </message>
 </context>
 <context>
@@ -240,95 +240,95 @@
         <location filename="../propDialog.ui" line="14"/>
         <location filename="../propDialog.cpp" line="15"/>
         <source>PDF Information</source>
-        <translation type="unfinished"></translation>
+        <translation>Podaci PDF-a</translation>
     </message>
     <message>
         <location filename="../propDialog.ui" line="20"/>
         <location filename="../propDialog.cpp" line="21"/>
         <source>Title:</source>
-        <translation type="unfinished"></translation>
+        <translation>Naslov:</translation>
     </message>
     <message>
         <location filename="../propDialog.ui" line="70"/>
         <location filename="../propDialog.cpp" line="22"/>
         <source>Subject:</source>
-        <translation type="unfinished"></translation>
+        <translation>Tema:</translation>
     </message>
     <message>
         <location filename="../propDialog.ui" line="120"/>
         <location filename="../propDialog.cpp" line="23"/>
         <source>Author:</source>
-        <translation type="unfinished"></translation>
+        <translation>Autor:</translation>
     </message>
     <message>
         <location filename="../propDialog.ui" line="170"/>
         <location filename="../propDialog.cpp" line="24"/>
         <source>Creator:</source>
-        <translation type="unfinished"></translation>
+        <translation>Izvorni program:</translation>
     </message>
     <message>
         <location filename="../propDialog.ui" line="223"/>
         <location filename="../propDialog.cpp" line="25"/>
         <source>Producer:</source>
-        <translation type="unfinished"></translation>
+        <translation>Izrađeno pomoću:</translation>
     </message>
     <message>
         <location filename="../propDialog.ui" line="270"/>
         <location filename="../propDialog.cpp" line="26"/>
         <source>Keywords:</source>
-        <translation type="unfinished"></translation>
+        <translation>Ključne riječi:</translation>
     </message>
     <message>
         <location filename="../propDialog.ui" line="329"/>
         <source>Created: </source>
-        <translation type="unfinished"></translation>
+        <translation>Stvoreno: </translation>
     </message>
     <message>
         <location filename="../propDialog.ui" line="388"/>
         <source>Modified: </source>
-        <translation type="unfinished"></translation>
+        <translation>Promijenjeno: </translation>
     </message>
     <message>
         <location filename="../propDialog.ui" line="447"/>
         <source>Page Size:</source>
-        <translation type="unfinished"></translation>
+        <translation>Veličina stranice:</translation>
     </message>
     <message>
         <location filename="../propDialog.ui" line="454"/>
         <source>Number of Pages:</source>
-        <translation type="unfinished"></translation>
+        <translation>Broj stranica:</translation>
     </message>
     <message>
         <location filename="../propDialog.ui" line="476"/>
         <location filename="../propDialog.cpp" line="31"/>
         <source>Save</source>
-        <translation type="unfinished"></translation>
+        <translation>Spremi</translation>
     </message>
     <message>
         <location filename="../propDialog.ui" line="483"/>
         <location filename="../propDialog.cpp" line="32"/>
         <source>Close</source>
-        <translation type="unfinished"></translation>
+        <translation>Zatvori</translation>
     </message>
     <message>
         <location filename="../propDialog.cpp" line="27"/>
         <source>Created:</source>
-        <translation type="unfinished"></translation>
+        <translation>Stvoreno:</translation>
     </message>
     <message>
         <location filename="../propDialog.cpp" line="28"/>
         <source>Modified:</source>
-        <translation type="unfinished"></translation>
+        <translation>Promijenjeno:</translation>
     </message>
     <message>
         <location filename="../propDialog.cpp" line="29"/>
         <source>Page Size: </source>
-        <translation type="unfinished"></translation>
+        <translation>Veličina stranice: </translation>
     </message>
     <message>
         <location filename="../propDialog.cpp" line="30"/>
         <source>Number of Pages: </source>
-        <translation type="unfinished"></translation>
+        <translation>Broj stranica: </translation>
     </message>
 </context>
 <context>
@@ -336,67 +336,67 @@
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="608"/>
         <source>Multimedia</source>
-        <translation type="unfinished"></translation>
+        <translation>Multimedija</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="609"/>
         <source>Development</source>
-        <translation type="unfinished"></translation>
+        <translation>Razvoj</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="610"/>
         <source>Education</source>
-        <translation type="unfinished"></translation>
+        <translation>Obrazovanje</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="611"/>
         <source>Games</source>
-        <translation type="unfinished"></translation>
+        <translation>Igre</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="612"/>
         <source>Graphics</source>
-        <translation type="unfinished"></translation>
+        <translation>Slike</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="613"/>
         <source>Network</source>
-        <translation type="unfinished"></translation>
+        <translation>Mreža</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="614"/>
         <source>Office</source>
-        <translation type="unfinished"></translation>
+        <translation>Ured</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="615"/>
         <source>Science</source>
-        <translation type="unfinished"></translation>
+        <translation>Znanost</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="616"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Postavke</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="617"/>
         <source>System</source>
-        <translation type="unfinished"></translation>
+        <translation>Sustav</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="618"/>
         <source>Utility</source>
-        <translation type="unfinished"></translation>
+        <translation>Uslužni program</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="619"/>
         <source>Wine</source>
-        <translation type="unfinished"></translation>
+        <translation>Wine</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="620"/>
         <source>Unsorted</source>
-        <translation type="unfinished"></translation>
+        <translation>Nerazvrstano</translation>
     </message>
 </context>
 </TS>
